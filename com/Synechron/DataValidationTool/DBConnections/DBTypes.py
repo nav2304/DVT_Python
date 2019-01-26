2ec55f627f3726464a756ce4861d37779287fae4
Vaijnath Palwade <palwadevm@users.noreply.github.com>
2019-01-27 04:32:48 +0530

    Commit Connection Updates to Git

--- com/Synechron/DataValidationTool/DBConnections/DBTypes.py
+++ com/Synechron/DataValidationTool/DBConnections/DBTypes.py
@@ -1,8 +1,11 @@
 # Enum To Create the definite connection Types
 # importing enum for enumerations 
 from enum import Enum
+
+
 # creating enumerations using class
 class ConnectionType(Enum):
-    dog = 1
-    cat = 2
-    lion = 3
+    Oracle = "Oracle"
+    SQLServer = "SQLServer"
+    MySQL = "MySQL"
+    File="File"

b691f5b4be2b9e720b2bee23e8a60d78331e0e3a
nav2304 <patil.nav@gmail.com>
2019-01-27 19:41:35 -0500

    Added pandas for reading csv files

---
+++ com/Synechron/DataValidationTool/DelimitedFileComparison/CSV_Reading.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv("F:\\Git_Projects\\DVT_Pyhon\\Files\\csv\\Source.csv")
+#df = 2
+print(df)
+
+
+print("This is the end of the program")
---
+++ com/Synechron/ToBeDeleted/DataTypes.py
@@ -0,0 +1,8 @@
+import numpy as np
+a = set(range(10))
+print(a)
+b = np.array(['Name',1001,25000.40])
+print(b)
+print(type(b))
+
+print("--This is end of the program--")
